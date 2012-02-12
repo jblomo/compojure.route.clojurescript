1bce932737fb3f51931025b3c678ccf5037a5112
Jim Blomo <jim.blomo+github@gmail.com>
2012-02-11 20:55:43 -0800

    update dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject compojure.route.clojurescript "0.3.5-SNAPSHOT"
+(defproject compojure.route.clojurescript "0.3.5"
   :description "Easily add a route to serve compiled ClojureScript"
-  :dependencies [[org.clojure/clojure "1.3.0-beta2"]
-                 [compojure "0.6.5" :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]
-                 [ring.middleware.clojurescript "0.5.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.7.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [compojure "1.0.1" :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]
+                 [ring.middleware.clojurescript "0.5.0"]])
 
